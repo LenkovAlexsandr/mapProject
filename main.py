4a218eb5a66c434ae9912fbfe9f929f212a44626
lenko <lenkovsaschka@gmail.com>
2024-02-03 22:31:43 +0300

    Задача 5

--- main.py
+++ main.py
@@ -1,59 +1,81 @@
 import os
 import sys
-
 import requests
 from PyQt5 import QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
+from functions_geocode import get_toponym, get_coordinates
 
 SCREEN_SIZE = [850, 500]
+
+
+class NoteText(QLineEdit):
+    def __init__(self, parent):
+        super(NoteText, self).__init__(parent)
+
+    def keyPressEvent(self, event):
+        print(12)
+        #self.QLineEdit.keyPressEvent(self, event)
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.coordinates = [44.269772, 46.307847]
         self.zoom = 15
         self.mod = ['map', 'sat', 'sat,skl']
         self.option = 0
-        self.address = [44.269772, 46.307847]
+        self.points = []
+        self.map_file = "map.png"
         self.get_image()
         self.initUI()
 
     def get_image(self):
         map_params = {
-            "ll": ','.join(list(map(str, self.address))),
+            "ll": ','.join(list(map(str, self.coordinates))),
             "z": self.zoom,
-            "l": self.mod[self.option]
+            "l": self.mod[self.option],
+            "pt": '~'.join(map(lambda x: x + ',vkbkm', self.points))
         }
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(map_api_server, params=map_params)
-        self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
+        self.points = []
+
+    def search(self):
+        point = get_coordinates(get_toponym(self.search_value.text()))
+        if point:
+            if point not in self.points:
+                self.points.append(point)
+            self.coordinates = list(map(float, point.split(',')))
+            self.get_image()
+            self.image.setPixmap(QPixmap(self.map_file))
 
     def keyPressEvent(self, event):
+        print(event)
         if event.key() == QtCore.Qt.Key_PageUp:
             self.zoom += 1 if self.zoom < 19 else 0
         elif event.key() == QtCore.Qt.Key_PageDown:
             self.zoom -= 1 if self.zoom > 1 else 0
         elif event.key() == QtCore.Qt.Key_Left:
-            self.address[0] -= (0.0025 * 2 ** (17 - self.zoom)) / 2
+            self.coordinates[0] -= (0.0025 * 2 ** (17 - self.zoom)) / 2
         elif event.key() == QtCore.Qt.Key_Up:
-            self.address[1] += (0.0025 * 2 ** (17 - self.zoom)) / 2
+            self.coordinates[1] += (0.0025 * 2 ** (17 - self.zoom)) / 2
         elif event.key() == QtCore.Qt.Key_Right:
-            self.address[0] += (0.0025 * 2 ** (17 - self.zoom)) / 2
+            self.coordinates[0] += (0.0025 * 2 ** (17 - self.zoom)) / 2
         elif event.key() == QtCore.Qt.Key_Down:
-            self.address[1] -= (0.0025 * 2 ** (17 - self.zoom)) / 2
+            self.coordinates[1] -= (0.0025 * 2 ** (17 - self.zoom)) / 2
         if event.key() == QtCore.Qt.Key_Space:
             self.option = (self.option + 1) % 3
-        if self.address[0] > 180:
-            self.address[0] = -360 + self.address[0]
-        elif self.address[0] < -180:
-            self.address[0] = 360 + self.address[0]
-        if self.address[1] > 85:
-            self.address[1] = -170 + self.address[1]
-        elif self.address[1] < -85:
-            self.address[1] = 170 + self.address[1]
+        if self.coordinates[0] > 180:
+            self.coordinates[0] = -360 + self.coordinates[0]
+        elif self.coordinates[0] < -180:
+            self.coordinates[0] = 360 + self.coordinates[0]
+        if self.coordinates[1] > 85:
+            self.coordinates[1] = -170 + self.coordinates[1]
+        elif self.coordinates[1] < -85:
+            self.coordinates[1] = 170 + self.coordinates[1]
         self.get_image()
         self.image.setPixmap(QPixmap(self.map_file))
 
@@ -65,6 +87,15 @@ class Example(QWidget):
         self.image = QLabel(self)
         self.image.resize(600, 450)
         self.image.setPixmap(QPixmap(self.map_file))
+
+        self.search_value = NoteText(self)
+        self.search_value.move(605, 10)
+        self.search_value.resize(240, 25)
+
+        self.search_button = QPushButton('Искать', self)
+        self.search_button.resize(80, 30)
+        self.search_button.move(765, 40)
+        self.search_button.clicked.connect(self.search)
 
     def closeEvent(self, event):
         os.remove(self.map_file)
